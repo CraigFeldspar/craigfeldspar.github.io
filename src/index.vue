1c60bd9213095e20e6da3c58cbfbefc81a808c90
Benjamin Guignabert <bguignabert@wanadev.fr>
2018-07-24 12:53:39 +0200

    adding basic index vue

--- src/index.vue
+++ src/index.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="example">{{ msg }}
-  </div>
+<b-container fluid> 
+    <b-row class="nav">
+        <b-col cols="12" offset-lg="1" lg="6" md="6" class="main-title">Anna Jeziorska</b-col>
+        <b-col sm class="custom-button">Contact</b-col>
+        <b-col sm class="custom-button">Projects</b-col>
+        <b-col sm class="custom-button">Blog</b-col>
+    </b-row>
+</b-container>
 </template>
 
 <script>
@@ -8,14 +14,30 @@
 export default {
   data () {
     return {
-      msg: 'Kocham that girl!'
     }
   }
 }
 </script>
 
 <style>
-.example {
-  color: red;
+.main-title {
+	font-size: 3em;
+	padding-left: 50px;
+}
+.nav {
+	height: 90px;
+	background: linear-gradient(to right, rgba(255,255,255,1) 0%,rgba(255,255,255,0) 100%);
+}
+.custom-button {
+	font-size: 2em;
+	line-height: 90px;
+	width: 150px;
+	text-align: center;
+	color: white;
+}
+body {
+	background-image: url(./background.jpg);
+	background-repeat: no-repeat;
+	background-size: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import Index from './index.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
 
 /* eslint-disable no-new */
 new Vue({
